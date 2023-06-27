61d7fb2bafc26176e9471c04b4dbf0d0e81bcbc9
captainlomik <nastiy248@mail.ru>
2023-06-28 00:11:38 +0300

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,21 +6,25 @@ import { RegistrationPageComponent } from './auth/registration-page/registration
 import { PostPageComponent } from './post-page/post-page.component';
 import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
 import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
+import { userGuard } from './shared/services/userGuard.service';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   {
     path: '', component: AuthLayoutComponent, children: [
       { path: '', redirectTo: '/login', pathMatch: 'full' },
       { path: 'login', component: LoginPageComponent },
-      { path: 'register', component: RegistrationPageComponent }
+      { path: 'register', component: RegistrationPageComponent },
     ]
   },
   {
-    path: '', component: SiteLayoutComponent, children: [
+    path: '', component: SiteLayoutComponent, canActivateChild: [userGuard], children: [
       { path: 'posts', component: MainPageComponent },
-      { path: 'post/:id', component: PostPageComponent }
+      { path: 'post/:id', component: PostPageComponent },
     ]
-  }
+  },
+  { path: '404', component: NotFoundComponent },
+  { path: '**', redirectTo: '/404', pathMatch: 'full' }
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MaterialModule } from './shared/material.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { PostBockComponent } from './post-bock/post-bock.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 @NgModule({
@@ -27,6 +28,7 @@ import { HttpClientModule } from '@angular/common/http';
     AuthLayoutComponent,
     SiteLayoutComponent,
     PostBockComponent,
+    NotFoundComponent,
 
 
   ],

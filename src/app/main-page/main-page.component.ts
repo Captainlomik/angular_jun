a15cf0c344289fa89bad0acd68ec63038604b70f
captainlomik <nastiy248@mail.ru>
2023-06-27 15:58:38 +0300

    fix main page view

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { PostsService } from '../shared/services/posts.service';
+import { Post } from '../shared/interfaces';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss']
 })
-export class MainPageComponent {
+export class MainPageComponent implements OnInit {
+
+  posts: Post[] = []
+
+  @Output() post:Post
+
+  constructor(private postService: PostsService) { }
+
+  ngOnInit() {
+    this.postService.getPosts().subscribe({
+      next: (posts:Post[]) => this.posts = posts,
+      error: e => console.log(e)
+    })
+  }
 
 }

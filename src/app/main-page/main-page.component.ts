0c77afedbcd2a33435ad7e15daca59714aba5a92
captainlomik <nastiy248@mail.ru>
2023-06-29 13:42:00 +0300

    add unSub

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,25 +1,31 @@
-import { Component, OnInit, Output } from '@angular/core';
+import { Component, OnDestroy, OnInit, Output } from '@angular/core';
 import { PostsService } from '../shared/services/posts.service';
 import { Post } from '../shared/interfaces';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss']
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent implements OnInit, OnDestroy {
 
   posts: Post[] = []
+  aSub: Subscription
 
-  @Output() post:Post
+  @Output() post: Post
 
   constructor(private postService: PostsService) { }
 
   ngOnInit() {
-    this.postService.getPosts().subscribe({
-      next: (posts:Post[]) => this.posts = posts,
+    this.aSub = this.postService.getPosts().subscribe({
+      next: (posts: Post[]) => this.posts = posts,
       error: e => console.log(e)
     })
+  }
+
+  ngOnDestroy() {
+    if (this.aSub) this.aSub.unsubscribe()
   }
 
 }
